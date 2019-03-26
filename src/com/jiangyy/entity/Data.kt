259b4f3f1f35ba49d3213558c4b854f36fb336d8
jiangyunyang <jiangyychn@163.com>
2019-03-26 17:26:35 +0800

    1.0.1

---
+++ src/com/jiangyy/entity/Data.kt
@@ -0,0 +1,246 @@
+package com.jiangyy.entity
+
+const val JSON_STR : String = "[\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"AndPermission\",\n" +
+        "    \"user\": \"yanzhenjie\",\n" +
+        "    \"version\": \"2.0.1\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.yanzhenjie.permission:support:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"AndroidUtilCode\",\n" +
+        "    \"user\": \"Blankj\",\n" +
+        "    \"version\": \"1.23.7\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.blankj:utilcode:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"BaseRecyclerViewAdapterHelper\",\n" +
+        "    \"user\": \"CymChad\",\n" +
+        "    \"version\": \"2.9.46\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.github.CymChad:BaseRecyclerViewAdapterHelper:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JitPack\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"butterknife\",\n" +
+        "    \"user\": \"JakeWharton\",\n" +
+        "    \"version\": \"10.1.0\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.jakewharton:butterknife:\",\n" +
+        "    \"processor\": \"com.jakewharton:butterknife-compiler:\",\n" +
+        "    \"maven\": \"MavenCentral\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"CircleImageView\",\n" +
+        "    \"user\": \"hdodenhof\",\n" +
+        "    \"version\": \"3.0.0\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"de.hdodenhof:circleimageview:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"fastjson\",\n" +
+        "    \"user\": \"alibaba\",\n" +
+        "    \"version\": \"1.2.55\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.alibaba:fastjson:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"fresco\",\n" +
+        "    \"user\": \"facebook\",\n" +
+        "    \"version\": \"1.13.0\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.facebook.fresco:fresco:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"glide\",\n" +
+        "    \"user\": \"bumptech\",\n" +
+        "    \"version\": \"4.9.0\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.github.bumptech.glide:glide:\",\n" +
+        "    \"processor\": \"com.github.bumptech.glide:compiler:\",\n" +
+        "    \"maven\": \"MavenCentral\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"gson\",\n" +
+        "    \"user\": \"google\",\n" +
+        "    \"version\": \"2.8.5\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.google.code.gson:gson:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"MavenCentral\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"logger\",\n" +
+        "    \"user\": \"orhanobut\",\n" +
+        "    \"version\": \"2.2.0\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.orhanobut:logger:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"lottie-android\",\n" +
+        "    \"user\": \"airbnb\",\n" +
+        "    \"version\": \"3.0.0\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.airbnb.android:lottie:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"MavenCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"MPAndroidChart\",\n" +
+        "    \"user\": \"PhilJay\",\n" +
+        "    \"version\": \"3.1.0\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.github.PhilJay:MPAndroidChart:v\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"jitpack\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"okhttp\",\n" +
+        "    \"user\": \"square\",\n" +
+        "    \"version\": \"3.14.0\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.squareup.okhttp3:okhttp:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"PhotoView\",\n" +
+        "    \"user\": \"chrisbanes\",\n" +
+        "    \"version\": \"2.3.0\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.github.chrisbanes:PhotoView:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"jitpack\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"picasso\",\n" +
+        "    \"user\": \"square\",\n" +
+        "    \"version\": \"2.71828\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.squareup.picasso:picasso:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"MavenCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"retrofit\",\n" +
+        "    \"user\": \"square\",\n" +
+        "    \"version\": \"2.5.0\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.squareup.retrofit2:retrofit:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"RxAndroid\",\n" +
+        "    \"user\": \"ReactiveX\",\n" +
+        "    \"version\": \"2.1.1\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"io.reactivex.rxjava2:rxandroid:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"RxJava\",\n" +
+        "    \"user\": \"ReactiveX\",\n" +
+        "    \"version\": \"2.2.7\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"io.reactivex.rxjava2:rxjava:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"MavenCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"SmartRefreshLayout\",\n" +
+        "    \"user\": \"scwang90\",\n" +
+        "    \"version\": \"1.1.0-alpha-21\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.scwang.smartrefresh:SmartRefreshLayout:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"smart-show\",\n" +
+        "    \"user\": \"the-pig-of-jungle\",\n" +
+        "    \"version\": \"2.6.6\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.github.the-pig-of-jungle.smart-show:toast:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JitPack\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"XPopup\",\n" +
+        "    \"user\": \"li-xiaojun\",\n" +
+        "    \"version\": \"1.5.2\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"com.lxj:xpopup:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"isChoose\": false,\n" +
+        "    \"name\": \"EventBus\",\n" +
+        "    \"user\": \"greenrobot\",\n" +
+        "    \"version\": \"3.1.1\",\n" +
+        "    \"latest_version\": \"\",\n" +
+        "    \"gradle\": \"org.greenrobot:eventbus:\",\n" +
+        "    \"processor\": \"\",\n" +
+        "    \"maven\": \"JCenter\",\n" +
+        "    \"remark\": \"\"\n" +
+        "  }\n" +
+        "]"
