b1cb0a94028ed481f422ecf53f9cb1f6c7f71003
jiangyunyang <jiangyychn@163.com>
2019-03-26 17:21:15 +0800

    1.0.1

--- src/com/jiangyy/action/AutoGradleAction.java
+++ src/com/jiangyy/action/AutoGradleAction.java
@@ -6,22 +6,31 @@ import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileEditor.impl.LoadTextUtil;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.jiangyy.entity.Repository;
 import com.jiangyy.ui.HomeDialog;
 import org.apache.commons.io.FileUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class AutoGradleAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        String path = getClass().getResource("/icon/alldata.json").getPath();
+
+        String path = getClass().getClassLoader().getResource("alldata.json").getFile();
+        showNotification(e, "path", "path", path);
         try {
             String input = FileUtils.readFileToString(new File(path), "UTF-8");
+            showNotification(e, "input", "input", input);
             List<Repository> data = JSON.parseArray(input, Repository.class);
             List<Repository> data0 = JSON.parseArray(input, Repository.class);
 
@@ -33,9 +42,18 @@ public class AutoGradleAction extends AnAction {
                 dialog.setVisible(true);
             }
         } catch (IOException e1) {
-            showNotification(e, "error", "提示", "项目列表解析失败");
+            showNotification(e, "error", "提示", e1.getMessage());
             e1.printStackTrace();
         }
+    }
+
+    private String inputStream2String(InputStream is) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int i = -1;
+        while ((i = is.read()) != -1) {
+            baos.write(i);
+        }
+        return baos.toString();
     }
 
     private void showNotification(@NotNull AnActionEvent e, String displayId, String title, String message) {
