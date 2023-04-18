d12a70a0c9bfb9ee676b579fa8fac986a0c7bf05
jiangyy <jiangyy@petalmail.com>
2023-04-18 11:06:31 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains:marketplace-zip-signer:0.1.8")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.json:json:20230227")
 }
 
 // Configure Gradle IntelliJ Plugin
